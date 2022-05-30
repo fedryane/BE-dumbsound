b0be320a94627eaceb349ffd14782a2f8f2b3a80
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-30 15:07:08 +0700

    fix add music cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,8 @@
-const clodudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2;
 clodudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = clodudinary;
+module.exports = cloudinary;
