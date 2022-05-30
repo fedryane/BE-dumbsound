10397635226fe230c1402e7f2b218675387cfa1a
Fedryan1337 <bandarisafedryan@gmail.com>
2022-05-30 20:31:41 +0700

    commit

--- src/controllers/music.js
+++ src/controllers/music.js
@@ -70,13 +70,47 @@ exports.getMusic = async (req, res) => {
 };
 
 // -------------------------------- add music ------------------------------//
-// exports.addMusic = async (req, res) => {
-//   try {
-//     // const result = await cloudinary.uploader.upload(req.file.path, {
-//     //   folder: "dumbsound_file",
-//     //   use_filename: true,
-//     //   unique_filename: false,
-//     // });
+
+exports.addMusic = async (req, res) => {
+  try {
+    const data = req.body;
+    const resultImage = await cloudinary.uploader.upload(req.files.thumbnail[0].path, {
+      folder: "dumbsound_file",
+      use_filename: true,
+      unique_filename: false,
+    });
+
+    const resultMusic = await cloudinary.uploader.upload(req.files.attache[0].path, {
+      folder: "dumbsound_file",
+      use_filename: true,
+      unique_filename: false,
+      resource_type: "raw",
+    });
+
+    const thumbnail = resultImage.public_id;
+    const attache = resultMusic.public_id;
+
+    const dataUpload = {
+      ...data,
+      thumbnail,
+      attache,
+    };
+
+    await music.create(dataUpload);
+    res.status(200).send({
+      status: "success",
+      message: "success add music",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(200).send({
+      status: "failed",
+      message: "Server error",
+    });
+  }
+};
+
+// ------------------------------------------ backup 1 ---------------------------------------- //
 
 //     const data = req.body;
 //     let newMusic = await music.create({
@@ -108,46 +142,48 @@ exports.getMusic = async (req, res) => {
 //   }
 // };
 
-exports.addMusic = async (req, res) => {
-  try {
-    const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "dumbmerch_file",
-      use_filename: true,
-      unique_filename: false,
-    });
+// ------------------------------------------ backup 2 ----------------------------------------//
 
-    const data = {
-      title: req.body.title,
-      year: req.body.year,
-      // artistId: req.user.id,
-      thumbnail: result.public_id,
-      attache: result.public_id,
-    };
+// exports.addMusic = async (req, res) => {
+//   try {
+//     const result = await cloudinary.uploader.upload(req.file.path, {
+//       folder: "dumbmerch_file",
+//       use_filename: true,
+//       unique_filename: false,
+//     });
 
-    let newMusic = await music.create(data);
+//     const data = {
+//       title: req.body.title,
+//       year: req.body.year,
+//       // artistId: req.user.id,
+//       thumbnail: result.public_id,
+//       attache: result.public_id,
+//     };
 
-    let musicData = await music.findOne({
-      where: {
-        id: newMusic.id,
-      },
-    });
-    musicData = JSON.parse(JSON.stringify(musicData));
-    res.status(200).send({
-      status: "success",
-      data: {
-        ...musicData,
-        thumbnail: process.env.FILE_PATH + musicData.thumbnail,
-        attache: process.env.FILE_PATH + musicData.attache,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.send({
-      status: "failed",
-      message: "failed to add Music",
-    });
-  }
-};
+//     let newMusic = await music.create(data);
+
+//     let musicData = await music.findOne({
+//       where: {
+//         id: newMusic.id,
+//       },
+//     });
+//     musicData = JSON.parse(JSON.stringify(musicData));
+//     res.status(200).send({
+//       status: "success",
+//       data: {
+//         ...musicData,
+//         thumbnail: process.env.FILE_PATH + musicData.thumbnail,
+//         attache: process.env.FILE_PATH + musicData.attache,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.send({
+//       status: "failed",
+//       message: "failed to add Music",
+//     });
+//   }
+// };
 
 // ------------------------------ update music ------------------------------//
 
